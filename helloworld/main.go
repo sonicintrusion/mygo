7ba20c85598fd0d75f9ac59c2342605c813388e4
khanh <khanh.nguyen@condenast.com>
2020-06-16 10:12:02 +0100

    adding helloworld

---
+++ helloworld/main.go
@@ -0,0 +1,8 @@
+package main // this identifies the code as an executable. any other name doesn't create one
+
+import "fmt"
+
+// must have one function for main
+func main() {
+	fmt.Println("Hi there!")
+}
