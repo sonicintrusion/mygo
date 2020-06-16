f56813fd88ff044fb0379463d0390a0f0fc7c761
khanh <khanh.nguyen@condenast.com>
2020-06-16 22:00:57 +0100

    Evens and odds

---
+++ evens/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var oddeven string
+	var values []int
+
+	for i := 0; i <= 10; i++ {
+		values = append(values, i)
+	}
+
+	for _, value := range values {
+		if value%2 == 0 {
+			oddeven = "even"
+		}
+		if value%2 != 0 {
+			oddeven = "odd"
+		}
+		fmt.Print(value)
+		fmt.Println(" is", oddeven)
+	}
+
+}
