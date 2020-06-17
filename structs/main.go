4e681d843869d706086c0c9bdc42a40f5a0070e1
khanh <khanh.nguyen@condenast.com>
2020-06-17 16:59:42 +0100

    adding strucs

---
+++ structs/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+)
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func main() {
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+	jim.print()
+
+	// jimPointer := &jim
+	// jimPointer.updateName("Jimmy")
+	jim.updateName("Jimmy")
+	jim.print()
+
+	fmt.Println("End")
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
